ac55581592aac6addf93654f477e239123843a50
Nacho <kulujun@gmail.com>
2023-10-10 06:59:31 +0800

    update

---
+++ ExamBankSystem/Helpers/WordHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamBankSystem.Helpers
+{
+    public class WordHelper
+    {
+        public static void ExportPaper(int paperId)
+        {
+
+        }
+    }
+}
